ad2a181551a6003b38a4486dd1f089b5a02164b4
christian-nieves <thechristiannieves@gmail.com>
2026-05-04 14:49:29 -0400

    Reservation class complete

---
+++ src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+public class Reservation {
+    private String roomType;
+    private double price;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+
+        if (roomType.equalsIgnoreCase("king")) {
+            this.price = 139;
+        } else if (roomType.equalsIgnoreCase("double")) {
+            this.price = 124;
+        }
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getReservationTotal() {
+        double totalPrice = price * numberOfNights;
+        if (isWeekend) {
+            totalPrice *= 1.1;
+        }
+        return totalPrice;
+    }
+
+}
+
